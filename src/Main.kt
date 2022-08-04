0d72b47ed35a211fd235123d366e3a404c262eb0
Ahmed Tarek <7madak10@gmail.com>
2022-08-05 00:20:01 +0200

    added a new function to compute sum

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,13 @@
 class Main {
+    fun addTwoNums(x : Int,y:Int): Int {
+        return x+y
+    }
 }
 fun main(){
     print(4+5)
+    val Main = Main()
+    var sum = Main.addTwoNums(4,5)
+    print(sum)
+
+
 }
